bcab9fcc3bb1e32c0adc58860e2204fbd60fee32
zkc <johnhwaston@163.com>
2017-07-05 05:32:56 -0700

    adding some htmls. Problem unsolved

--- src/main/java/com/zkc/zkcwenda/dao/UserDAO.java
+++ src/main/java/com/zkc/zkcwenda/dao/UserDAO.java
@@ -5,18 +5,19 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+
 /**
  * Created by zkc on 17/6/28.
  */
 @Repository
 @Mapper
 public interface UserDAO {
-    String Table_Name = " user ";
+    String TABLE_NAME = " user ";
     String INSERT_FILEDS = " name, password, salt, headUrl ";
     String SELECT_FIELDS = "id," + INSERT_FILEDS;
 
-    @Insert({"insert into", Table_Name, "(", INSERT_FILEDS,
-            ") values(#{name},#{password},#{salt},#{headUrl})"})
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FILEDS,
+            ") values (#{name},#{password},#{salt},#{headUrl})"})
     int addUser(User user);
 
 }
